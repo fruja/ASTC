53489a3e3017ca05da016227077f3cfc4494a5aa
fruja <frida.jakobsen@hotmail.com>
2018-11-19 14:45:44 +0100

    getting event description

--- JS/scripts.js
+++ JS/scripts.js
@@ -55,17 +55,17 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = event.EventTitle;
 
-      /*const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;*/
+      const p = document.createElement('p');
+      event.EventDesc = event.EventDesc.substring(0, 300);
+      p.textContent = `${event.EventDesc}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
-      /*card.appendChild(p);*/
+      card.appendChild(p);
     });
   } else {
     const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
+    errorMessage.textContent = `Not working!`;
     app.appendChild(errorMessage);
   }
 }
