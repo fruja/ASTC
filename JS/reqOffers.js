09178299735d5fce051b2d795017826df330d5fd
fruja <frida.jakobsen@hotmail.com>
2018-11-28 14:08:58 +0100

    images added

--- JS/reqOffers.js
+++ JS/reqOffers.js
@@ -26,29 +26,29 @@ sendReq("http://localhost:55825/Api/Offers", function processResponse(response) 
     var data = JSON.parse(response);
 
     data.forEach(offers => {
-        //makes a new 'a' tag for every event (like this: <a class="card" href="#"> </a>)
+        //makes a new 'a' tag for every offer (like this: <a class="card" href="#"> </a>)
         card = document.createElement('a');
         card.setAttribute('class', 'card');
         card.setAttribute('id', offers.ID);
-        card.setAttribute('href', './singleEvent.html#' + offers.ID)
+        card.setAttribute('href', './singleOffer.html#' + offers.ID)
 
-        //makes a new 'img' tag for the image of the event
+        //makes a new 'img' tag for the image of the offer
         var image = document.createElement('img');
         image.setAttribute('src', offers.OfferImg);
-        image.setAttribute('class', 'eventImg');
+        image.setAttribute('class', 'offerImg');
 
-        //makes a new 'h3' tag for the title of the event
+        //makes a new 'h3' tag for the title of the offer
         var offerTitle = document.createElement('h3');
         offerTitle.textContent = offers.OfferTitle;
 
-        //makes a new 'p' tag for the description of the event
+        //makes a new 'p' tag for the description of the offer
         var offerDescription = document.createElement('p');
         offerDescription.textContent = offers.OfferDesc;
 
         //adds the 'a' tag to the 'offerList' div
         offerList.appendChild(card);
         
-        //adds the information about the event in to the 'a' tag
+        //adds the information about the offer in to the 'a' tag
         card.appendChild(image);
         card.appendChild(offerTitle);
         card.appendChild(offerDescription);
@@ -56,22 +56,22 @@ sendReq("http://localhost:55825/Api/Offers", function processResponse(response) 
 });
 
 //Finds the current ID of the URL
-/*var pageURL = window.location.href;
-var CurrentID = pageURL.substr(pageURL.lastIndexOf('/') + 18);*/
+var pageURL = window.location.href;
+var CurrentID = pageURL.substr(pageURL.lastIndexOf('/') + 18);
 
-//GET single event by the current ID
-/*sendReq(`http://localhost:55825/Api/Offers/${CurrentID}`, function processResponse(response) {
+//GET single offer by the current ID
+sendReq(`http://localhost:55825/Api/Offers/${CurrentID}`, function processResponse(response) {
     var singleOffer = document.getElementById("singleOffer");
     singleOffer.innerHTML = "";
 
     var data = JSON.parse(response);
 
-    var eventTitle = document.createElement('h3');
-    eventTitle.textContent = data.EventTitle;
+    var offerTitle = document.createElement('h3');
+    offerTitle.textContent = data.OfferTitle;
 
-    var eventDescription = document.createElement('p');
-    eventDescription.textContent = data.EventDesc;
+    var offerDescription = document.createElement('p');
+    offerDescription.textContent = data.OfferDesc;
 
-    singleOffer.appendChild(eventTitle);
-    singleOffer.appendChild(eventDescription);
-});*/
+    singleOffer.appendChild(offerTitle);
+    singleOffer.appendChild(offerDescription);
+});
