9afb6340f4210d8d3ee3105c9e231398763a12f5
fruja <frida.jakobsen@hotmail.com>
2018-12-05 14:54:46 +0100

    better validation

--- JS/signUp.js
+++ JS/signUp.js
@@ -1,5 +1,4 @@
 function Register() {
-    var url = "http://localhost:55825/Api/Customers/CreateCustomerMember";
 
     //The ID's of the imputs in th form from 'signUp.html'
     var customer = {
@@ -17,31 +16,34 @@ function Register() {
     var lname = document.forms["registerForm"]["lname"].value;
     var email = document.forms["registerForm"]["email"].value;
     var pwd = document.forms["registerForm"]["pwd"].value;
+    var text = document.getElementById("validationInfo");
 
     if (fname == "") {
-        alert("First name must be filled out");
+        //alert("First name must be filled out");
+        text.innerHTML = "First name must be filled out";
         return false;
     } else if (lname == "") {
-        alert("Name must be filled out");
+        text.innerHTML = "Last name must be filled out";
         return false;
     } else if (email == "") {
-        alert("Email must be filled out");
+        text.innerHTML = "Email must be filled out";
         return false;
     } else if (pwd == "") {
-        alert("Password must be filled out");
+        text.innerHTML = "Password must be filled out";
         return false;
     } else {
-        console.log("Successfully registered");
+        text.innerHTML = "Successfully registered";
     }
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
+    xhr.open("POST", "http://localhost:55825/Api/Customers/CreateCustomerMember", true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.onload = function () {
         var users = JSON.parse(xhr.responseText);
-        if (xhr.readyState == 4 && xhr.status == "201") {
+        if (xhr.readyState == 4 && xhr.status == "200") {
             console.table(users);
             window.location.href = "./user.html";
+            document.getElementById("registerForm").reset();
         } else {
             console.error(users);
         }
