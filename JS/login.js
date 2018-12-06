fb783d2dc5b8ba7826b325cabd5c16645aba9a75
fruja <frida.jakobsen@hotmail.com>
2018-12-06 13:21:19 +0100

    comments to login.js

--- JS/login.js
+++ JS/login.js
@@ -1,47 +1,41 @@
+function Login() {
+    //Get the value from the email and password
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#pwd').value;
 
-        
-        function Login() {
+    //Convert the email and password to JSON
+    var json = JSON.stringify(email, password);
 
-            var email = document.querySelector('#email').value;
-            var password = document.querySelector('#pwd').value;
+    //Validate email and password
+    var valEmail = document.forms["loginForm"]["email"].value;
+    var valPwd = document.forms["loginForm"]["pwd"].value;
+    var text = document.getElementById("validationInfo");
 
+    //checks if the values are empty
+    if (valEmail == "") {
+        text.innerHTML = "Email must be filled out";
+        return false;
+    } else if (valPwd == "") {
+        text.innerHTML = "Password must be filled out";
+        return false;
+    }
 
-            var json = JSON.stringify(email, password);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", `http://localhost:55825/api/Customers/LoginCheck/${email}/${password}`, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onload = function () {
+        var userData = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            sessionStorage.setItem("UserID", userData); //Gets the data of the person who logged in (ID)
+            window.location.href = "./user.html"; //Sends the user to the user page
 
-            //Validation
-            var valEmail = document.forms["loginForm"]["email"].value;
-            var valPwd = document.forms["loginForm"]["pwd"].value;
-            var text = document.getElementById("validationInfo");
+            text.innerHTML = "Succesfully login!";
+            document.getElementById("loginForm").reset(); //reset the inputs
 
-            if (valEmail == "") {
-                text.innerHTML = "Email must be filled out";
-                return false;
-            } else if (valPwd == "") {
-                text.innerHTML = "Password must be filled out";
-                return false;
-            } 
-
-
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", `http://localhost:55825/api/Customers/LoginCheck/${email}/${password}`, true);
-            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            xhr.onload = function () {
-                var userData = JSON.parse(xhr.responseText);
-                console.log (userData)
-                if (xhr.readyState == 4 && xhr.status == "200") {
-                    console.log ('Success')
-                    sessionStorage.setItem("UserID", userData);
-                    //document.getElementById("tester").innerHTML = sessionStorage.getItem("UserID");
-                    window.location.href = "./user.html";
-                    text.innerHTML = "Succesfully login!";
-
-
-                    document.getElementById("loginForm").reset();
-
-                } else {
-                    text.innerHTML = "Email or password is incorrect, please try again";
-                }
-            }
-            xhr.send(json);
-        
+        } else {
+            text.innerHTML = "Email or password is incorrect, please try again";
         }
+    }
+    xhr.send(json);
+
+}
