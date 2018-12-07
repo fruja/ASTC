f641b16c1457c4a9c21e8c4b1af5ab4f004dd09e
fruja <frida.jakobsen@hotmail.com>
2018-12-07 14:37:11 +0100

    comments done

--- JS/signUp.js
+++ JS/signUp.js
@@ -1,5 +1,4 @@
 function Register() {
-
     //The ID's of the imputs in th form from 'signUp.html'
     var customer = {
         FName: document.querySelector('#fname').value,
@@ -9,6 +8,7 @@ function Register() {
         Credit: 500
     }
 
+    //Convert the email and password to JSON
     var json = JSON.stringify(customer);
 
     //Validation
@@ -16,10 +16,12 @@ function Register() {
     var lname = document.forms["registerForm"]["lname"].value;
     var email = document.forms["registerForm"]["email"].value;
     var pwd = document.forms["registerForm"]["pwd"].value;
+
+    //Tell the user that something went wrong
     var text = document.getElementById("validationInfo");
 
+    //check if the values are empty
     if (fname == "") {
-        //alert("First name must be filled out");
         text.innerHTML = "First name must be filled out";
         return false;
     } else if (lname == "") {
@@ -41,15 +43,12 @@ function Register() {
     xhr.onload = function () {
         var users = JSON.parse(xhr.responseText);
         if (xhr.readyState == 4 && xhr.status == "200") {
-            sessionStorage.setItem("UserID", users);
-            console.table(users);
-            window.location.href = "./user.html";
-            document.getElementById("registerForm").reset();
-            
+            sessionStorage.setItem("UserID", users); //Gets the data of the person who signed up (ID)
+            window.location.href = "./user.html"; //Sends the user to the user page
+            document.getElementById("registerForm").reset(); //reset the inputs
         } else {
-            console.error(users);
+            console.log("Sign up error");
         }
     }
     xhr.send(json);
-
 }
