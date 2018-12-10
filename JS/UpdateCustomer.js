9091ddb663eda82d7fccd92284c46af1a2b8ee96
Christian Bo Petersen <1061823@ucn.dk>
2018-12-10 13:15:23 +0100

    dasfklsdflksdf

--- JS/UpdateCustomer.js
+++ JS/UpdateCustomer.js
@@ -1,58 +1,3 @@
-/*function Update() {
-    //The ID's of the imputs in th form from 'signUp.html'
-    var customer = {
-        FName: document.querySelector('#fname').value,
-        LName: document.querySelector('#lname').value,
-        Email: document.querySelector('#email').value,
-        Pass: document.querySelector('#pwd').value,
-    }
-
-    //Convert the email and password to JSON
-    var json = JSON.stringify(customer);
-
-    //Validation
-    var fname = document.forms["registerForm"]["fname"].value;
-    var lname = document.forms["registerForm"]["lname"].value;
-    var email = document.forms["registerForm"]["email"].value;
-    var pwd = document.forms["registerForm"]["pwd"].value;
-
-    //Tell the user that something went wrong
-    var text = document.getElementById("validationInfo");
-
-    //check if the values are empty
-    if (fname == "") {
-        text.innerHTML = "First name must be filled out";
-        return false;
-    } else if (lname == "") {
-        text.innerHTML = "Last name must be filled out";
-        return false;
-    } else if (email == "") {
-        text.innerHTML = "Email must be filled out";
-        return false;
-    } else if (pwd == "") {
-        text.innerHTML = "Password must be filled out";
-        return false;
-    } else {
-        text.innerHTML = "Successfully Updated";
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:55825/Api/Customers/UpdateCustomer", true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.onload = function () {
-        var users = JSON.parse(xhr.responseText);
-        if (xhr.readyState == 4 && xhr.status == "200") {
-            sessionStorage.setItem("UserID", users);
-            window.location.href = "./user.html";
-            document.getElementById("registerForm").reset();
-        } else {
-            console.log("Update user error");
-        }
-    }
-    xhr.send(json);
-
-}*/
-
 //Check if the URL you are trying to get is OK
 function sendReq(url, callbackFunction) {
     var xmlhttp
@@ -76,6 +21,7 @@ function sendReq(url, callbackFunction) {
 //Gets the ID of the user that is logged in
 var CurrentID = sessionStorage.getItem("UserID");
 
+//Gets the current users info and puts them as the value of the input fields
 sendReq(`http://localhost:55825/Api/Customers/${CurrentID}`, function processResponse(response) {
     var getData = JSON.parse(response);
 
@@ -88,6 +34,8 @@ document.getElementById("pwd").value = getData.Pass;
 
 //Updates User
 function updateUser() {
+
+    //Gets the current users info to make sure that ID,Credit and Barcode dont update.
     sendReq(`http://localhost:55825/Api/Customers/${CurrentID}`, function processResponse(response) {
     var getData = JSON.parse(response);
 
@@ -129,6 +77,7 @@ function updateUser() {
     
     var json = JSON.stringify(data);
 
+    //PUT request to update user.
     var xhr = new XMLHttpRequest();
     xhr.open("PUT", `http://localhost:55825/Api/Customers/${CurrentID}`, true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
