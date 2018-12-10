d093ba493a9ebff9556eb3aa12da47c0c492554d
fruja <frida.jakobsen@hotmail.com>
2018-12-10 13:22:54 +0100

    finpussning

--- JS/UpdateCustomer.js
+++ JS/UpdateCustomer.js
@@ -25,72 +25,70 @@ var CurrentID = sessionStorage.getItem("UserID");
 sendReq(`http://localhost:55825/Api/Customers/${CurrentID}`, function processResponse(response) {
     var getData = JSON.parse(response);
 
-document.getElementById("fname").value = getData.Fname;
-document.getElementById("lname").value = getData.Lname;
-document.getElementById("email").value = getData.Email;
-document.getElementById("pwd").value = getData.Pass;
+    document.getElementById("fname").value = getData.Fname;
+    document.getElementById("lname").value = getData.Lname;
+    document.getElementById("email").value = getData.Email;
+    document.getElementById("pwd").value = getData.Pass;
 });
 
 
 //Updates User
 function updateUser() {
 
-    //Gets the current users info to make sure that ID,Credit and Barcode dont update.
+    //Gets the current users info to make sure that ID, Credit and Barcode don't update
     sendReq(`http://localhost:55825/Api/Customers/${CurrentID}`, function processResponse(response) {
-    var getData = JSON.parse(response);
+        var getData = JSON.parse(response);
 
-    // Update a user
-    var data = {};
-    data.ID = CurrentID;
-    data.FName = document.querySelector('#fname').value;
-    data.LName = document.querySelector('#lname').value;
-    data.Email = document.querySelector('#email').value;
-    data.Pass = document.querySelector('#pwd').value;
-    data.Credit = getData.Credit;
-    data.Barcode = getData.Barcode;
+        // Update a user
+        var data = {};
+        data.ID = CurrentID;
+        data.FName = document.querySelector('#fname').value;
+        data.LName = document.querySelector('#lname').value;
+        data.Email = document.querySelector('#email').value;
+        data.Pass = document.querySelector('#pwd').value;
+        data.Credit = getData.Credit;
+        data.Barcode = getData.Barcode;
 
-    //Validation
-    var fname = document.forms["updateForm"]["fname"].value;
-    var lname = document.forms["updateForm"]["lname"].value;
-    var email = document.forms["updateForm"]["email"].value;
-    var pwd = document.forms["updateForm"]["pwd"].value;
+        //Validation
+        var fname = document.forms["updateForm"]["fname"].value;
+        var lname = document.forms["updateForm"]["lname"].value;
+        var email = document.forms["updateForm"]["email"].value;
+        var pwd = document.forms["updateForm"]["pwd"].value;
 
-    //Tell the user that something went wrong
-    var text = document.getElementById("validationInformation");
+        //Tell the user that something went wrong
+        var text = document.getElementById("validationInformation");
 
-    //check if the values are empty
-    if (fname == "") {
-        text.innerHTML = "First name must be filled out";
-        return false;
-    } else if (lname == "") {
-        text.innerHTML = "Last name must be filled out";
-        return false;
-    } else if (email == "") {
-        text.innerHTML = "Email must be filled out";
-        return false;
-    } else if (pwd == "") {
-        text.innerHTML = "Password must be filled out";
-        return false;
-    } else {
-        text.innerHTML = "Successfully updated";
-    }
-    
-    var json = JSON.stringify(data);
-
-    //PUT request to update user.
-    var xhr = new XMLHttpRequest();
-    xhr.open("PUT", `http://localhost:55825/Api/Customers/${CurrentID}`, true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.onload = function () {
-
-        if (xhr.readyState == 4 && xhr.status == "204") {
-            console.log("Success");
-           // console.table(users);
+        //check if the values are empty
+        if (fname == "") {
+            text.innerHTML = "First name must be filled out";
+            return false;
+        } else if (lname == "") {
+            text.innerHTML = "Last name must be filled out";
+            return false;
+        } else if (email == "") {
+            text.innerHTML = "Email must be filled out";
+            return false;
+        } else if (pwd == "") {
+            text.innerHTML = "Password must be filled out";
+            return false;
         } else {
-            console.log("Error");
-           // console.error(users);
-        }   
-    }
-    xhr.send(json);
-});
+            text.innerHTML = "Successfully updated";
+        }
+
+        var json = JSON.stringify(data);
+
+        //PUT request to update user
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", `http://localhost:55825/Api/Customers/${CurrentID}`, true);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.onload = function () {
+            //4 means that the operation is complete and 204 means No Content (we don't get anything sent back)
+            if (xhr.readyState == 4 && xhr.status == "204") {
+                console.log("Success");
+            } else {
+                console.log("Error");
+            }
+        }
+        xhr.send(json);
+    });
 }
